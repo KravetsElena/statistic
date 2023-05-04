1e612e24cbc7fa648df664df5e947cf23784bb24
ElenaKr <Kravets_2024@internet.ru>
2023-05-04 11:40:43 +0500

    JaCoCo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -11,8 +11,22 @@ public class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
 
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxOtherData() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        // чтобы задействовать изменение переменной currentMax,
+        // меняем значение первого месяца на отличное от максимального
+
+        long expected = 12;
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
